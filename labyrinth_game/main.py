99463797916c5936a57d60810a179a55876fc598
Setrence <Setrence1@yandex.ru>
2025-10-26 00:02:04 +0300

    2.1. Architecture and data generation

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
+from constants import ROOMS
+
 def main():
-    print("Hello this shit project")
+    game_state = {
+        'player_inventory': [],  # Инвентарь игрока
+        'current_room': 'entrance',  # Текущая комната
+        'game_over': False,  # Значения окончания игры
+        'steps_taken': 0  # Количество шагов
+    }
