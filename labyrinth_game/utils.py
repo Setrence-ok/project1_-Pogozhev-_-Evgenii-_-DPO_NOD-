7a4c7aa2439ee0680b6492e6112380cb1f5be2b4
Setrence <Setrence1@yandex.ru>
2025-10-26 16:34:48 +0300

    Redacted function describe_current_room

--- labyrinth_game/utils.py
+++ labyrinth_game/utils.py
@@ -4,8 +4,19 @@ from .constants import rooms
 
 
 def describe_current_room(game_state):
+    room_exit = []
     room = game_state.get('current_room')
     print(f"== {room.upper()} ==")
-    for k, v in rooms[room].items():
-            print(f"{v}")
+    print(f"{rooms[room]['description']}")
+    for exit_direction, exit_room in rooms[room]['exits'].items():
+        room_exit.append(exit_direction)
+    if rooms[room]['items']:
+        print("Заметные предметы: " + (", ").join(rooms[room]['items']))
+    else:
+        print("Предметов в комнате нет")
+    print("Выходы: " + (", ").join(room_exit))
 
+    if rooms[room]['puzzle']:
+        print("Кажется, здесь есть загадка (используйте команду solve).")
+    else:
+        print("Загадок в комнате нет")
